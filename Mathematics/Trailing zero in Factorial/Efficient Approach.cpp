31dd0f9239b035bfaa4195bc604ca270bdd0c7b5
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-08-24 11:49:45 +0530

    Create Efficient Approach.cpp

---
+++ Mathematics/Trailing zero in Factorial/Efficient Approach.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int res=0;
+    for(int i=5;i<=n;i=i*5)
+    {
+        res=res+n/i;
+    }
+    cout<<res;
+
+    return 0;
+}
