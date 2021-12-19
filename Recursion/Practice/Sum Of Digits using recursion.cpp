f6179517eaf10be9c8eb90fbeaf661612c103297
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-12-19 09:22:03 +0530

    Create Sum Of Digits using recursion.cpp

---
+++ Recursion/Practice/Sum Of Digits using recursion.cpp
@@ -0,0 +1,26 @@
+Given a number, N. Find the sum of all the digits of N
+ 
+
+Example 1:
+
+Input:
+N = 12
+Output:
+3
+Explanation:
+Sum of 12's digits:
+1 + 2 = 3
+
+Solution:
+Given a number, N. Find the sum of all the digits of N
+ 
+
+Example 1:
+
+Input:
+N = 12
+Output:
+3
+Explanation:
+Sum of 12's digits:
+1 + 2 = 3
